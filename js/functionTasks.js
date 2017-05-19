9d3098252305cf645c99cb3d373cd9fd15da02d0
puzatkin-ivan <puzatkin-ivan@users.noreply.github.com>
2017-05-19 20:20:10 +0300

    Add files via upload
    Исправил задание 4 и форматирование.

--- js/functionTasks.js
+++ js/functionTasks.js
@@ -1,42 +1,43 @@
-
 function printName() {
-  var planetName = "Земля";
-  var userName = "Василий";
+  "use strict";
+
+  let planetName = "Земля";
+  let userName = "Василий";
 
   console.log("Задание 1");
   console.log(planetName, userName);
 }
 
 function authorization() {
+  "use strict";
 
-  var user;
-    user = {
-      login: prompt("Введите Ваш Логин", ""),
-      password: ''
-    };
+  let user;
+  user = {
+    login: prompt("Введите Ваш Логин", ""),
+    password: ''
+  }
 
   if (user.login == "Админ") {
     user.password = prompt("Введите Ваш Пароль", "");
 
     if (user.password == "Тёмный Властелин") {
-        alert("Добро Пожаловать");
+      alert("Добро Пожаловать");
     } else if (user.password == null) {
-       alert("Вход отменён");
+      alert("Вход отменён");
     } else {
-       alert("Пароль Неверен");
+      alert("Пароль Неверен");
     }
-
   } else if (user.login == null) {
-      alert("Вход Отменён");
+    alert("Вход Отменён");
   } else {
-      alert("Я Тебя не знаю");
+    alert("Я Тебя не знаю");
   }
 }
 
-function showEven() {
-  var digit = 0;
+function printEvenWhile() {
+  "use strict";
+  let digit = 0;
 
-  alert("Задание 3 в консоли");
   console.log("Задание 3");
 
   while (digit != 10) {
@@ -45,24 +46,62 @@ function showEven() {
   }
 }
 
-function showDigit() {
-  var digit;
-  var previusDigit;
+function printEvenFor() {
+  "use strict";
+  console.log("Задание 3.1");
 
-  alert("Задание 4 в консоли");
+  for (let digit = 2; digit <= 10; digit++) {
+    if (digit % 2 == 0) {
+      console.log(digit);
+    }
+  }
+}
+
+function checkDigit(firstDigit, secondDigit) {
+  
+  firstDigit = +firstDigit;
+  secondDigit = +secondDigit
+  
+  if ((firstDigit) && (secondDigit)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function showPrimeDigit(beginInterval, endInterval) {
+
   console.log("Задание 4");
 
-  Prime:
-    for (digit = 2; digit <= 10; digit++ ) {
-      for (previusDigit = 2; previusDigit < digit; previusDigit++){
-        if (digit % previusDigit == 0) continue Prime;
-      }
+  beginInterval = +beginInterval;
+  endInterval = +endInterval;
 
-      console.log(digit);
-   }
+  for (beginInterval; beginInterval <= endInterval; beginInterval++) {
+    let divider = 2;
+    let prime = true;
+
+    for (divider; ((beginInterval > divider) && (prime)); divider++) {
+      if (beginInterval % divider === 0) {
+        prime = false;
+      }
+    }
+
+    if (prime) {
+      console.log(beginInterval);
+    }
+  }
 }
 
 printName();
 authorization();
-showEven();
-showDigit();
+printEvenWhile();
+printEvenFor();
+
+let beginingInterval = prompt("Введите начало интервала", '');
+let endInterval = prompt("Введите конец интервала", '');
+
+if (checkDigit(beginingInterval, endInterval)) {
+  showPrimeDigit(beginingInterval, endInterval);
+} else {
+  console.log("Данные введены некорректно");
+}
