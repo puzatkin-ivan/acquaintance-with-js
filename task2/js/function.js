ee906c4662cd2aa8e9741cb86a2b149458a03926
puzatkin-ivan <puzatkin-ivan@users.noreply.github.com>
2017-05-20 23:42:12 +0300

    Add files via upload
    Реализовал основные задания. Добавления элемента списка "Item N", где N - это его порядковый номер (при нажатии кнопки "Add"). И очистка списка (при нажатии кнопки  "Clean").

---
+++ task2/js/function.js
@@ -0,0 +1,21 @@
+function addListItem() {
+  let parent = document.getElementById("list");
+  let numberItem = ++parent.childNodes.length;
+  let li = document.createElement('li');
+  let inputItem = document.createElement('input');
+  let textElem = document.createTextNode('Item');
+  let textElemNum = document.createTextNode(numberItem);
+ 
+ 
+  li.appendChild(textElem);
+  li.appendChild(textElemNum);
+  parent.appendChild(li);
+}
+
+function cleanList() {
+  let parent = document.getElementById("list");
+
+  while (parent.childNodes.length != 0) {
+    parent.removeChild(parent.lastChild);
+  }
+}
