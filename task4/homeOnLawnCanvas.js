6319d78c3c6cb51cd5ab251c60c58a497970a6ed
puzatkin-ivan <puzatkin1998@gmail.com>
2017-06-06 12:59:19 +0300

    Animated Cloud

--- task4/homeOnLawnCanvas.js
+++ task4/homeOnLawnCanvas.js
@@ -7,36 +7,43 @@ let canvasContext = canvas.getContext("2d");
 canvas.width = WIDTH_CANVAS;
 canvas.height = HEIGHT_CANVAS;
 
-createSky();
-showSun();
-moveCloud(50, 50);
-createLawn();
-createHome();
-showRabbit();
 
-function createSky() {
-  canvasContext.fillStyle = "#86b0db";
-  canvasContext.fillRect(0, 0, WIDTH_CANVAS, 350);
+function moveCloud(xCloud) {
+  drawCloud(xCloud, 50);
+  drawCloud(xCloud + 100, 75);
+  drawCloud(xCloud + 50, 150);
 }
+let xCloud = 50;
+let timeBetweenJump = 50;
+let animatedCanvas = function() {
+  drawAll();
 
+  moveCloud(xCloud);
 
-function moveCloud(x, y) {
-  showCloud(x, y);
-  showCloud(x + 70, y + 50);
-  showCloud(x + 175, y + 40);
-  showCloud(x + 250, y + 10);
-  showCloud(x + 300, y + 70);
-  showCloud(x + 400, y + 35);
-  showCloud(x + 500, y + 20);
-  showCloud(x + 600, y + 10);
-  showCloud(x + 700, y);
-  showCloud(x + 800, y + 35);
-  showCloud(x + 900, y + 10);
-}
+  timeBetweenJump -= 1;
+  xCloud += 2;
 
-function showCloud(xCloud, yCloud) {
+  if (timeBetweenJump === 0) {
+    jumpRabbit();
+    timeBetweenJump = 50;
+  } else {
+    drawRabbit(250);
+  }
+
+  if (xCloud > 1100) {
+    xCloud = 0;
+    requestAnimationFrame(animatedCanvas);
+  } else {
+    requestAnimationFrame(animatedCanvas);
+  }
+};
+
+animatedCanvas();
+
+function drawCloud(xCloud, yCloud) {
   canvasContext.beginPath();
   canvasContext.fillStyle = "white";
+  canvasContext.strokeStyle = "white";
   canvasContext.moveTo(xCloud, yCloud);
   canvasContext.quadraticCurveTo(xCloud - 25, yCloud - 35, xCloud + 40, yCloud - 20);
   canvasContext.quadraticCurveTo(xCloud + 105, yCloud - 45, xCloud + 75, yCloud + 15);
@@ -44,24 +51,52 @@ function showCloud(xCloud, yCloud) {
   canvasContext.quadraticCurveTo(xCloud - 25, yCloud + 20, xCloud, yCloud);
   canvasContext.closePath();
   canvasContext.fill();
+  canvasContext.stroke();
 }
 
-function showSun() {
-    canvasContext.arc(1100, 60, 45, 0, 2 * Math.PI);
-    canvasContext.fillStyle = '#e1ff00';
-    canvasContext.fill();
+function jumpRabbit() {
+  let yRabbit = 250;
+
+  for (let timeJumpTop = 100; timeJumpTop !== 0; timeJumpTop--) {
+    yRabbit -= 1;
+    drawRabbit(yRabbit);
+  }
 }
 
-function createLawn() {
-    canvasContext.fillStyle = "#4ee239";
-    canvasContext.fillRect(0, 350, WIDTH_CANVAS, 450);
-    createHerb(10, 500);
-    createHerb(550, 400);
-    createHerb(600, 500);
-    createHerb(800, 450);
+function drawRabbit(yRabbit) {
+  let rabbit = document.getElementById("rabbit");
+
+  canvasContext.drawImage(rabbit, 0, 0, 350, 500, 800, yRabbit, 300, 450);
 }
 
-function createHerb(x, y) {
+function drawAll(){
+  drawSky();
+  drawSun();
+  drawLawn();
+  drawHome();
+}
+
+function drawSky() {
+  canvasContext.fillStyle = "#86b0db";
+  canvasContext.fillRect(0, 0, WIDTH_CANVAS, 350);
+}
+
+function drawSun() {
+  canvasContext.arc(1100, 60, 45, 0, 2 * Math.PI);
+  canvasContext.fillStyle = '#e1ff00';
+  canvasContext.fill();
+}
+
+function drawLawn() {
+  canvasContext.fillStyle = "#4ee239";
+  canvasContext.fillRect(0, 350, WIDTH_CANVAS, 450);
+  drawHerb(10, 500);
+  drawHerb(550, 400);
+  drawHerb(600, 500);
+  drawHerb(800, 450);
+}
+
+function drawHerb(x, y) {
   canvasContext.beginPath();
   canvasContext.moveTo(x, y);
   canvasContext.strokeStyle = "#1dab28";
@@ -75,51 +110,39 @@ function createHerb(x, y) {
 }
 
 
-function createHome() {
+function drawHome() {
     canvasContext.fillStyle = "#923b13";
     canvasContext.fillRect(150, 300, 350, 300);
-    showWindow();
-    createHousetop();
+    drawWindow();
+    drawHousetop();
 }
 
-function showWindow() {
+function drawWindow() {
   for (let numberWindow = 3; numberWindow !== 0; --numberWindow) {
-    createWindow();
+    canvasContext.fillStyle = "white";
+    canvasContext.fillRect(195, 375, 65, 105);
     canvasContext.translate(100, 0);
   }
 
   canvasContext.translate(-300, 0);
 }
 
-function createWindow() {
-    canvasContext.fillStyle = "white";
-    canvasContext.fillRect(195, 375, 65, 105);
+function drawHousetop() {
+  drawChimney();
+  canvasContext.beginPath();
+  canvasContext.fillStyle = "#371b07";
+  canvasContext.strokeStyle = "#000000";
+  canvasContext.lineWidth = 5;
+  canvasContext.moveTo(325, 120);
+  canvasContext.lineTo(125, 300);
+  canvasContext.lineTo(525, 300);
+  canvasContext.lineTo(325, 120);
+  canvasContext.fill();
+  canvasContext.stroke();
 }
 
-function createHousetop() {
-    createChimney();
-    canvasContext.beginPath();
-    canvasContext.fillStyle = "#371b07";
-    canvasContext.strokeStyle = "#000000";
-    canvasContext.lineWidth = 5;
-    canvasContext.moveTo(325, 120);
-    canvasContext.lineTo(125, 300);
-    canvasContext.lineTo(525, 300);
-    canvasContext.lineTo(325, 120);
-    canvasContext.fill();
-    canvasContext.stroke();
+function drawChimney() {
+  canvasContext.fillStyle = "brown";
+  canvasContext.fillRect(195, 145, 35, 110);
 }
 
-function createChimney() {
-    canvasContext.fillStyle = "brown";
-    canvasContext.fillRect(195, 145, 35, 110);
-}
-
-function showRabbit() {
-    let rabbit = new Image();
-
-    rabbit.src = 'spriteCanvas.png';
-    rabbit.addEventListener("load", function() {
-      canvasContext.drawImage(rabbit, 0, 0, 300, 450, 800, 250, 250, 350)
-    });
-}
